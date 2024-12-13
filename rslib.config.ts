6933071b140190af6c2062b82141fa9f318584d2
mohongen <496585582@qq.com>
2024-12-13 11:49:39 +0800

    feat: test alias

--- rslib.config.ts
+++ rslib.config.ts
@@ -1,29 +1,45 @@
-import { defineConfig } from "@rslib/core";
-import { pluginVue } from "@rsbuild/plugin-vue";
-import { pluginVueJsx } from "@rsbuild/plugin-vue-jsx";
-import { pluginBabel } from "@rsbuild/plugin-babel";
+import { defineConfig } from '@rslib/core';
+import { pluginVue } from '@rsbuild/plugin-vue';
+import { pluginVueJsx } from '@rsbuild/plugin-vue-jsx';
+import { pluginBabel } from '@rsbuild/plugin-babel';
 
 export default defineConfig({
-    source: {
-        entry: {
-            index: ["./src/**"],
-        },
-    },
-    lib: [
-        {
-            bundle: false,
-            dts: true,
-            format: "esm",
-        },
-    ],
-    output: {
-        target: "web",
-    },
-    plugins: [
-        pluginBabel({
-            include: /\.(?:jsx|tsx)$/,
-        }),
-        pluginVue(),
-        pluginVueJsx(),
-    ],
+  resolve: {
+    alias: {
+      '@': './src'
+    }
+  },
+  source: {
+    entry: {
+      index: ['./src/**']
+    }
+  },
+  lib: [
+    {
+      bundle: false,
+      dts: true,
+      format: 'esm'
+    }
+  ],
+  output: {
+    target: 'web'
+  },
+  plugins: [
+    pluginBabel({
+      include: /\.(?:jsx|tsx)$/
+    }),
+    pluginVue(),
+    pluginVueJsx()
+  ],
+  tools: {
+    rspack: {
+      plugins: [
+        require('unplugin-vue-macros/rspack')({
+          // 覆盖插件选项
+          vue: pluginVue(),
+          vueJsx: pluginVueJsx()
+        })
+      ]
+    }
+  }
 });
---
+++ src/components/Input.vue
@@ -0,0 +1,29 @@
+<script setup lang="tsx">
+import { ref } from 'vue';
+const props = withDefaults(
+  defineProps<{
+    defaultValue: string;
+  }>(),
+  {
+    defaultValue: ''
+  }
+);
+const { defaultValue } = props;
+const handleInput = (e) => {
+  console.info(e.target.value);
+  value.value = e.target.value;
+};
+const value = ref(defaultValue);
+
+defineRender(() => {
+  return (
+    <div>
+      <input
+        onInput={handleInput}
+        value={value.value}
+      />
+    </div>
+  );
+});
+</script>
+<style lang="scss" scoped></style>
